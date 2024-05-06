04dc664a7f2d9091fecb1dd6060d0e428044673a
CD36930 <152339976+CD36930@users.noreply.github.com>
2024-05-05 22:25:49 -0400

    Add files via upload
    upload final site version

---
+++ scripts/index.js
@@ -0,0 +1,21 @@
+//Javascript for Little Sparrow Music Studio- lsms/index.html 
+
+/*global document: false */
+
+function showMenu() {
+    "use strict";
+    document.getElementById("myDropdown").classList.toggle("show");
+}
+
+document.getElementById("hamburger").addEventListener("click", showMenu);
+
+
+
+
+
+
+
+
+
+
+
